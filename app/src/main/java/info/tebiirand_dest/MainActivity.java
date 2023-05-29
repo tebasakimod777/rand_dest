d02f3c19d829e352db80e4f8db59f21502981800
tebii <tebasaki.wejobs@gmail.com>
2023-05-29 23:50:55 +0900

    change geolocation calculation

--- app/src/main/java/info/tebiirand_dest/MainActivity.java
+++ app/src/main/java/info/tebiirand_dest/MainActivity.java
@@ -16,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -23,11 +24,12 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity implements LocationListener, View.OnClickListener {
 
     private LocationManager mLocationManager;
-    Random x_rand;
-    Random y_rand;
+    Random rand;
     private double user_x = 36.1085765;
     private double user_y = 140.0998895;
     private int mLocationType;
+    private EditText min_box;
+    private EditText max_box;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,10 +37,11 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
         setContentView(R.layout.activity_main);
 
         Button startButton = (Button) findViewById(R.id.start_button);
+        min_box = (EditText) findViewById(R.id.mim_distance);
+        max_box = (EditText) findViewById(R.id.max_distance);
         startButton.setOnClickListener(this);
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        x_rand = new Random();
-        y_rand = new Random();
+        rand = new Random();
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED
@@ -114,7 +117,13 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
     @Override
     public void onClick(View view) {
         if (view.getId() == R.id.start_button) {
-            @SuppressLint("DefaultLocale") String url = String.format("https://www.google.co.jp/maps/place/%f,%f", x_rand.nextDouble() + user_x, y_rand.nextDouble() + user_y);
+            double min = Double.parseDouble(min_box.getText().toString()) / 111.;
+            double max = Double.parseDouble(max_box.getText().toString()) / 111.;
+            double r = rand.nextDouble() * (max-min) + min;
+            double theta = rand.nextDouble() * 2 * Math.PI;
+            double dest_x = r * Math.cos(theta) + user_x;
+            double dest_y = r * Math.sin(theta) + user_y;
+            @SuppressLint("DefaultLocale") String url = String.format("https://www.google.co.jp/maps/place/%f,%f", dest_x, dest_y);
             Log.d("PlaceSample", url);
             Uri uri = Uri.parse(url);
             Intent intent = new Intent(Intent.ACTION_VIEW, uri);
